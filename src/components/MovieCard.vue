b1d190ee7830153617e40953b1eb6e9b9fed1321
AgoDe <ago.defelice@gmail.com>
2022-02-03 13:01:59 +0100

    create a moviecard component

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,38 @@
+<template>
+     <div class="movie-card">
+                <img src="" alt="">
+
+                <div class="movie-info">
+                    <div>
+                        <span>Titolo:</span>
+                        <span>The Avengers</span>
+                    </div>
+                    <div>
+                        <span>Titolo originale:</span>
+                        <span>The Avengers</span>
+                    </div>
+                    <div>
+                        <span>voto:</span>
+                        <span>5</span>
+                    </div>
+
+                </div>
+            </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+    .movie-card {
+        width: 200px;
+        height: 300px;
+
+        border: white 1px solid;
+    }
+    
+</style>
