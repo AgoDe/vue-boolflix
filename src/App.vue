f53d752dc86e6b5f155a2a2a85aefb59d7436403
AgoDe <ago.defelice@gmail.com>
2022-02-03 11:33:59 +0100

    principal layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
     
+    <!-- header container -->
+    <header-container/>
+    <!-- / header container -->
+
+    <!-- main container -->
+    <main-container/>
+    <!-- / main container -->
+
   </div>
 </template>
 
 <script>
+import HeaderContainer from './components/HeaderContainer.vue'
+import MainContainer from './components/MainContainer.vue'
 
 
 export default {
+  components: {
+    HeaderContainer,
+    MainContainer 
+  },
 
 }
 </script>
 
 <style lang="scss">
 
-</style>
+</styl
+HeaderContainere>
