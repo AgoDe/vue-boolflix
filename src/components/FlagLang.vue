feed12a8055c4b88a43f58145dcc2febc9b8f376
AgoDe <ago.defelice@gmail.com>
2022-02-07 15:49:05 +0100

    fix languages

--- src/components/FlagLang.vue
+++ src/components/FlagLang.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="flag"
     :class="language">
+        {{ language }}
     </div>
 </template>
 
@@ -19,33 +20,44 @@ export default {
     width: 37px;
     height: 25px;
     margin: 0 auto;
-    background: url('../assets/download.png') center center;
+    // background: url('../assets/download.png') center center;
     background-size: cover;
 
  }
  .flag.en {
     background: url(../assets/united-kingdom-flag.png) center center no-repeat;
     background-size: contain;
+    color: transparent;
  }
  .flag.it {
     background: url(../assets/italy-flag.png) center center no-repeat;
     background-size: contain;
+    color: transparent;
+
  }
  .flag.fr {
     background: url(../assets/france-flag.png) center center no-repeat;
     background-size: contain;
+    color: transparent;
+
  }
  .flag.es {
     background: url(../assets/spain-flag.png) center center no-repeat;
     background-size: contain;
+    color: transparent;
+
  }
  .flag.ko {
     background: url(../assets/south-korea-flag.png) center center no-repeat;
     background-size: contain;
+    color: transparent;
+
  }
  .flag.de {
     background: url(../assets/germany-flag.png) center center no-repeat;
     background-size: contain;
+    color: transparent;
+
  }
 
 </style>
